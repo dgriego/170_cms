048b44d7813722dfb16c9ebca99694bb66ef04ea
dgriego <daniel@jackrabbitsystems.com>
2016-10-10 12:00:40 -0600

    adds data and views directories adds route to display file contents

--- cms.rb
+++ cms.rb
@@ -4,3 +4,22 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'tilt/erubis'
 require 'pry'
+
+root = File.expand_path("..", __FILE__)
+
+get '/' do
+  # data = Dir.open('data/')
+  # @files = data.select { |file| file if File.extname(file) == '.txt' }
+
+  @files = Dir.glob(root + "/data/*").map do |path|
+    File.basename(path)
+  end
+
+  erb :documents_list
+end
+
+get '/:filename' do
+  content_type 'text/plain'
+  file_path = "#{root}/data/#{params[:filename]}"
+  File.read(file_path)
+end
